2278f57b75200b7cd7ae2503fd3b0fb9934a5f7a
Bhammond9 <142949554+Bhammond901@users.noreply.github.com>
2024-12-20 19:09:00 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// const links = document.querySelectorAll('p a');
+
+// for(let link of links) {
+//     console.log(link.href)
+// }
+
+// const allLinks = document.querySelectorAll('a');
+
+// for (let link of allLinks){
+//     link.innerText = 'I am a link!!'
+// }
+
+for (let link of allLinks) {
+    link.style.color = 'rgb(0, 108, 134)';
+    link.style.textDecorationColor = 'magenta'; 
+}
